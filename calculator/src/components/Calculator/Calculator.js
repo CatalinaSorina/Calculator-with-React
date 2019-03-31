207172a2835b4d76123e8982f826596d56ffe366
Gherzan Catalina-Sorina <cgherzan@frigoglass.com>
2019-03-31 20:59:43 +0300

    Calculator component base

---
+++ calculator/src/components/Calculator/Calculator.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import DisplayResult from '../DisplayResult/DisplayResult';
+import NumberButton from '../NumberButton/NumberButton';
+import ActionButton from '../ActionButton/ActionButton';
+
+const Calculator = props => {
+    return (
+        <div>
+            <DisplayResult />
+        </div>
+    )
+}
+
+export default Calculator;
